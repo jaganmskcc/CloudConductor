dbd9ac8104a68b9274d82bd1c4b27ddbbc539e1b
Razvan Panea <razvan.panea@duke.edu>
2018-12-13 14:30:50 -0500

    Fix the final output bug for modules that return None

--- System/Graph/TaskWorker.py
+++ System/Graph/TaskWorker.py
@@ -163,12 +163,14 @@ class TaskWorker(Thread):
                 # Post-process command output if necessary
                 self.module.process_cmd_output(out, err)
 
-                # Save output files in workspace output dirs (if any)
+                # Set the status to finalized
                 self.set_status(self.FINALIZING)
-                output_files        = self.datastore.get_task_output_files(self.task.get_ID())
-                final_output_types  = self.task.get_final_output_keys()
-                if len(output_files) > 0:
-                    self.module_executor.save_output(output_files, final_output_types)
+
+            # Save output files in workspace output dirs (if any)
+            output_files        = self.datastore.get_task_output_files(self.task.get_ID())
+            final_output_types  = self.task.get_final_output_keys()
+            if len(output_files) > 0:
+                self.module_executor.save_output(output_files, final_output_types)
 
             # Indicate that task finished without any errors
             if not self.__cancelled:
